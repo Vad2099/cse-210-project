bd93f44dd161669a03416851594cd47735ebf90e
Vad2099 <valdemarlasnibat@hotmail.es>
2024-01-21 04:33:03 -0600

    Develop02 finished

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> _prompts;
+    
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+}
