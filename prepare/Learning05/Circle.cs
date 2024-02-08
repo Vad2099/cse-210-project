ae356c4e8f3211c8056548ea55894b78e2fc8cfc
Vad2099 <valdemarlasnibat@hotmail.es>
2024-02-07 22:32:23 -0600

    Learning 5 finished

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Circle : Shape
+{
+    private double _radius;
+    
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+
+
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red",5);
+        
+
+        Rectangle rectangle = new Rectangle("green", 5, 8);
+        
+
+        Circle circle = new Circle("black",10);
+    
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"color: {color} and area: {area}");
+        }
+        
+
+
+    
     }
 }
