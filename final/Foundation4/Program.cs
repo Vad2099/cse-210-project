8847004d81bdfe73848205f888b587831493e4e8
Vad2099 <valdemarlasnibat@hotmail.es>
2024-02-20 22:45:33 -0600

    Foundation 4 finished

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("Feb 12, 2024", 30, 4.8);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("Feb 14, 2024", 60, 15);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("Feb 17, 2024", 20, 20);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
